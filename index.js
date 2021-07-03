9837e508aecc9a9e2c30940d9c423cbc7a9f7cd7
DarKWinGTM <tonmaster.1.08@hotmail.com>
2021-07-04 03:02:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -335,21 +335,21 @@ async function packedtrx(DATA){
                 }
             ]
         }; 
-
-        const transaction   = { ...transaction, actions: await api.serializeActions(transaction.actions) };
-        const serial        = api.serializeTransaction(transaction);
+	    
+        const transactions  = { ...transaction, actions: await api.serializeActions(transaction.actions) };
+        const serial        = api.serializeTransaction(transactions);
         const packed_trx    = arrayToHex(serial); 
         
-        //	const result        = await api.transact(transaction, { broadcast: false, sign: false });
-        //	const abis          = await api.getTransactionAbis(transaction);
-        //	const requiredKeys  = privateKeys.map((privateKey) => PrivateKey.fromString(privateKey).getPublicKey().toString());
-        //	const packed_trx    = arrayToHex(result.serializedTransaction); 
+        //  const result        = await api.transact(transaction, { broadcast: false, sign: false });
+        //  const abis          = await api.getTransactionAbis(transaction);
+        //  const requiredKeys  = privateKeys.map((privateKey) => PrivateKey.fromString(privateKey).getPublicKey().toString());
+        //  const packed_trx    = arrayToHex(result.serializedTransaction); 
         //  console.log(result);
         //  console.log(packed_trx); 
         //  console.log(result.serializedTransaction.toString()); 
 
         return new Promise(function(resolve, reject) {
-            resolve({packed_trx, serializedTransaction : serial, transaction, serial}); 
+            resolve({packed_trx, serializedTransaction : serial, transactions, serial}); 
         });
 
 /*!
