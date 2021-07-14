b60fde9c00faa1b5b9321111f1be714b316e4d9c
DarKWinGTM <tonmaster.1.08@hotmail.com>
2021-07-15 05:33:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -255,7 +255,7 @@ async function mine(DATA){
             hash = null;
         }; 
         
-        if (itr >= 100000 * 10){
+        if (itr >= 100000 * 4){
             rand_arr    = ''; 
             hex_digest  = `SORRY WE CAN NOT SOLVED LOOP ${ itr }`; 
             break; 
